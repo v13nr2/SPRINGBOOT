b7309b47b3a38185b80176392d64ce4b903fb8a7
v13nr <nanang.manajer@gmail.com>
2020-06-09 04:50:30 +0800

    another door jwt

---
+++ app-nanang-git/src/main/java/com/nanang/app/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.nanang.app.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nanang.app.model.DAOUser;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+	
+	DAOUser findByUsername(String username);
+	
+}
---
+++ app-nanang-git/src/main/java/com/nanang/app/dao/package-info.java
@@ -0,0 +1,1 @@
+package com.nanang.app.dao;
