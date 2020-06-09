74abec5eab6fb16eeccdd5ef154d517d813cdabf
v13nr <nanang.manajer@gmail.com>
2020-06-09 09:23:16 +0800

    Login by MySQL Cred.

--- app-nanang-git/src/main/java/com/nanang/app/controller/TokenController.java
+++ app-nanang-git/src/main/java/com/nanang/app/controller/TokenController.java
@@ -3,6 +3,7 @@ package com.nanang.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.nanang.app.dto.ProductDto;
 import com.nanang.app.model.DataResponse;
 import com.nanang.app.model.TokenRequest;
 import com.nanang.app.model.TokenResponse;
+import com.nanang.app.service.JwtUserDetailsService;
 import com.nanang.app.service.TokenService;
 import com.nanang.app.utility.Konstanta;
 
@@ -24,7 +26,9 @@ public class TokenController {
 	@Autowired
 	private TokenService tokenService;
 	
-	
+
+	@Autowired
+	private JwtUserDetailsService userDetailsService;
 	
 	@GetMapping("/getToken")
 	public ResponseEntity<TokenResponse> getToken(
@@ -41,8 +45,17 @@ public class TokenController {
 
 	@PostMapping("getTokenPost")
 	public ResponseEntity<TokenResponse> getToken(@RequestBody TokenRequest token){
-		TokenResponse response = tokenService.getToken(token);
-		return new ResponseEntity<TokenResponse>(response,HttpStatus.OK);
+		
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(token.getClientId());
+		if(userDetails.getUsername() != null) {
+			TokenResponse response = tokenService.getToken(token);
+			return new ResponseEntity<TokenResponse>(response,HttpStatus.OK);
+		} else {
+
+			TokenResponse response = tokenService.getToken(token);
+			return new ResponseEntity<TokenResponse>(response,HttpStatus.BAD_REQUEST);
+		}
 		
 	}
 }
